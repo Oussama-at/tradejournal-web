c3bb140dab0d1c3408dbb6ef0a79f5a880c3b1ad
oussama <oussamawinn100@gmail.com>
2026-06-27 02:04:44 +0100

    wi

--- src/pages/setupTests.js
+++
@@ -1,7 +0,0 @@
-/**
- * src/setupTests.js
- *
- * Loaded automatically by react-scripts before every test file.
- * Adds custom matchers from @testing-library/jest-dom.
- */
-import '@testing-library/jest-dom';
